ab96e9125d23b33239423039b0f555a5453a5b86
43880377 <tashi.chhering.kc@gmail.com>
2022-02-10 13:25:28 +0545

    added view for table

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,72 +1,27 @@
 <!DOCTYPE html>
 <html>
 <style>
-  th,
-  td {
-    padding: 5px;
-  }
+th,td {
+  padding: 5px;
+}
 </style>
-
 <body>
 
-  <h2>The XMLHttpRequest Object</h2>
+<h2>The XMLHttpRequest Object</h2>
 
-  <form action="">
-    <div class="form-group">
+<form action=""> 
+  <select name="customers" onchange="showCustomer(this.value)">
+    <option value="">Select a customer:</option>
+    <option value="ALFKI">Alfreds Futterkiste</option>
+    <option value="NORTS ">North/South</option>
+    <option value="WOLZA">Wolski Zajazd</option>
+  </select>
+</form>
+<br>
+<div id="txtHint">Customer info will be listed here...</div>
 
-      <label>Faculty:</label>
-
-      <select name="customers" onchange="showCustomer(this.value)">
-        <option value="">Select a customer:</option>
-        <option value="ALFKI">Alfreds Futterkiste</option>
-        <option value="NORTS ">North/South</option>
-        <option value="WOLZA">Wolski Zajazd</option>
-      </select>
-
-    </div>
-
-
-    <div class="form-group">
-
-      <label>Select State:</label>
-      <select name="id_state" class-"form-control">
-        <option value="">Select a customer:</option>
-      </select>
-
-    </div>
-
-
-    <div class="form-group">
-
-      <button class="btn btn-success" type="submit">Submit</button>
-
-    </div>
-  </form>
-  <br>
-  <div id="txtHint">Customer info will be listed here...</div>
-
-  
-</body>
 <script>
-    function showCustomer() {
-
-      var id_country = $(this).val();
-      <?php echo 'hello'?>
-      $.ajax({
-
-        url: "<?php echo view('test') ?>",
-
-        method: 'GET',
-
-        data: {
-          id_country: id_country},
-
-        success: function(data) {
-
-          <?php echo "True here"?>
-        }
-
-      });
-    }
-  </script>
+  $document
+</script>
+</body>
 </html>
